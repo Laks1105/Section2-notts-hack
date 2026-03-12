64efd65f22298e844873cbc6ca3ff2ff5975cd6d
rinpatch <rin@wantan.me>
2026-03-12 16:51:55 +0800

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,9 @@ export const metadata: Metadata = {
   title: "Notts Hack 2026 | Blockchain Hackathon",
   description: "Build. Hack. Decentralize. Join Notts Hack, a blockchain-themed hackathon by CSS at University of Nottingham. April 6-12, 2026.",
   keywords: ["hackathon", "blockchain", "web3", "university of nottingham", "CSS", "coding", "decentralized"],
+  icons: {
+    icon: "/favicon.png",
+  },
   openGraph: {
     title: "Notts Hack 2026",
     description: "Build. Hack. Decentralize. A blockchain-themed hackathon.",
